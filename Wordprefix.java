61565a42b166ca39e8820bf3ea5dac870cae7555
kathirgoogle <kathir.twwi@gmail.com>
2025-01-16 22:07:58 +0530

    Add files via upload
    Version 1 of Program which extracts words containing certain Prefix

---
+++ Wordprefix.java
@@ -0,0 +1,36 @@
+package january;
+
+import java.util.*;
+import java.util.regex.*;
+public class Wordprefix {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String words[] = {"pay","attention","practice","attend","atlast"};
+		System.out.println("The no of words are "+ prefixcount( words , "at") );	
+	}
+	static public int prefixcount(String [] words, String prefix)
+	{	 
+		int cnt = 0,i=0,j=0;
+		int len = words.length;
+		
+		String temp = "at";
+		String words1[] = new String[5];
+		prefix = "^["+ temp +"].*";
+		String words2 = Arrays.toString(words);
+		System.out.println(words2);
+		while(i != len ) {
+		Pattern p = Pattern.compile(prefix);
+		Matcher M = p.matcher(words1[i]);
+		
+		if( M.matches())
+		{
+		 words1[j] = words[i];
+		 System.out.println(words1[j]);
+		 cnt++;
+		}
+		j++;i++;
+		}
+		
+		return cnt;
+	}
+}
