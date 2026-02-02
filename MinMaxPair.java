c7e6f123fa05103e0c67829337a0ed41fcbc4321
kathirgoogle <kathir.twwi@gmail.com>
2026-02-02 05:53:43 +0530

    Remove unused parameter from minPairSum method

--- MinMaxPair.java
+++ MinMaxPair.java
@@ -3,15 +3,16 @@ import java.util.*;
 import java.util.stream.Collectors;
 public class MinMaxPair {
 
-public static int minPairSum(int[] nums, Integer obj) {
+public static int minPairSum(int[] nums ) {
        int [] num = nums.clone();
        int len = num.length;
        int j=0,k=0,s=0; k = len/2 ;
        List  list1 = Arrays.stream(num).boxed()
     		   		.sorted(Comparator.naturalOrder()).collect(Collectors.toList());
-       List list2 = list1.stream().map(	i-> {  } );
+       
     	    
        return 0;	
 }
 	
 }
+
